a7d2b5245c89ea66b87502cd5b2252409942e010
Anqwert <Anqwert8@proton.me>
2024-04-15 12:48:08 +0530

    demo changes

--- client/src/utils/accordion.jsx
+++ client/src/utils/accordion.jsx
@@ -5,7 +5,7 @@ const data = [
     icon: <HiShieldCheck/>,
     heading: "Best interest rates on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Discover the best interest rates on the market for your real estate ventures. Whether you're seeking residential properties, commercial spaces, or investment opportunities, we provide access to competitive rates tailored to your needs. Start browsing now to find the perfect financing options for your next venture.",
   },
   {
     icon: <MdCancel />,
